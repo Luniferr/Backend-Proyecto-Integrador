149dae5ab16248a8f081f20ad364887ed0c48673
lunapazfernanda <luna.gamero@gmail.com>
2023-11-23 03:17:48 -0300

    casi ready

--- src/models/subscriptionModel.js
+++ src/models/subscriptionModel.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-
 const subscriptionSchema = new Schema({
   email: {
     type: String,
     require: true,
-    unique: true
+    unique: true,
   },
 });
-
 
 const Subscription = mongoose.model("Subscription", subscriptionSchema);
 
