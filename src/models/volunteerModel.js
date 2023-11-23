15e1bd985b26e2f8ab214029627d1d89e6049fd4
lunapazfernanda <luna.gamero@gmail.com>
2023-11-22 23:44:57 -0300

    frontend de volunteer form conectado

--- src/models/volunteerModel.js
+++ src/models/volunteerModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-
 const volunteerSchema = new Schema({
   name: {
     type: String,
@@ -14,7 +13,7 @@ const volunteerSchema = new Schema({
     unique: true,
   },
   age: {
-    type: String,
+    type: Number,
     require: true,
   },
   email: {
@@ -22,23 +21,64 @@ const volunteerSchema = new Schema({
     require: true,
   },
   phone: {
-    type: String,
+    type: Number,
     require: true,
   },
   ocupation: {
     type: String,
     require: true,
+    Option: {
+      value: "Estudiante",
+      value: "Trabajador",
+      value: "Jubilado",
+      value: "Otro",
+    },
   },
   residence: {
     type: String,
     require: true,
+    Option: {
+      value: "Santiago",
+      value: "Providencia",
+      value: "LasCondes",
+      value: "LaReina",
+      value: "Ñuñoa",
+      value: "Macul",
+      value: "Peñalolen",
+      value: "LaFlorida",
+      value: "PuenteAlto",
+      value: "Maipu",
+      value: "Renca",
+      value: "Quilicura",
+      value: "Huechuraba",
+      value: "Recoleta",
+      value: "Independencia",
+      value: "Conchalí",
+      value: "Vitacura",
+      value: "LoBarnechea",
+      value: "Peñaflor",
+      value: "Pudahuel",
+      value: "CerroNavia",
+      value: "QuintaNormal",
+      value: "LoPrado",
+      value: "EstacionCentral",
+      value: "LoEspejo",
+      value: "PedroAguirre",
+      value: "SanMiguel",
+      value: "SanJoaquin",
+      value: "LaGranja",
+      value: "LaCisterna",
+      value: "ElBosque",
+      value: "SanBernardo",
+      value: "LaPintana",
+      value: "Colina",
+    },
   },
   motivation: {
     type: String,
     require: true,
   },
 });
-
 
 const Volunteer = mongoose.model("Volunteer", volunteerSchema);
 
